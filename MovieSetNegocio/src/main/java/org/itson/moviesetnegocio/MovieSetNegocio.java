c4f30a82d699ff94fa20bf6c5c71bfb1d267d5f4
Wogcas <gael.castro247887@potros.itson.edu.mx>
2024-11-17 22:00:48 -0700

    negocios

---
+++ MovieSetNegocio/src/main/java/org/itson/moviesetnegocio/MovieSetNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.moviesetnegocio;
+
+/**
+ *
+ * @author castr
+ */
+public class MovieSetNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
