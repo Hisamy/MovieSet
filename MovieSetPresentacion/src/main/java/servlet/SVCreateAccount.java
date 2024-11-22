3c751356a3ad51350c1ee0321894efcbe92ed1e8
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-11-21 22:39:07 -0700

    create account

--- MovieSetPresentacion/src/main/java/servlet/SVCreateAccount.java
+++ MovieSetPresentacion/src/main/java/servlet/SVCreateAccount.java
@@ -1,5 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
 package servlet;
 
+import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
@@ -7,41 +12,52 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Part;
-import java.io.File;
-import java.io.IOException;
 
-/**
- *
- * @author Hisamy Cinco Cota
- * @author Asiel Apodaca Monge
- * @author Gael Rafael Castro Molina
- */
-@MultipartConfig(
-        fileSizeThreshold = 1024 * 1024 * 2, // 2MB
-        maxFileSize = 500 * 1024 * 10, // 10MB
-        maxRequestSize = 1024 * 1024 * 50 // 50MB
-)
-
-@WebServlet(name = "SVCreateAccount", urlPatterns = {"/CreateAccount"})
+@MultipartConfig
+@WebServlet(name = "SVCreateAccount", urlPatterns = {"/SVCreateAccount"})
 public class SVCreateAccount extends HttpServlet {
 
     /**
-     * Maneja solicitudes POST enviadas al servlet, procesa datos del
-     * formulario, valida contraseñas y guarda la imagen del avatar en el
-     * servidor.
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
      *
-     * @param request el objeto HttpServletRequest que contiene la solicitud del
-     * cliente.
-     * @param response el objeto HttpServletResponse para enviar la respuesta al
-     * cliente.
-     * @throws ServletException si ocurre un error específico del servlet.
-     * @throws IOException si ocurre un error de E/S durante el manejo de la
-     * solicitud.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
         //Obtener los datos del formulario
         String username = request.getParameter(
                 "username");
@@ -53,87 +69,20 @@ public class SVCreateAccount extends HttpServlet {
                 "repeat-password");
 //        Part filePart = request.getPart(
 //                "avatar");
-        System.out.println("Username: "+username);
-        System.out.println("Email: "+username);
-        System.out.println("Password: "+username);
-        System.out.println("RepeatPassword: "+username);
-        
-//        try {
-//            saveImage(filePart);
-//
-//            if (password.equals(repeatPassword)) {
-//                successAlert(request, response);
-//            } else {
-//                request.setAttribute("message", "Passwords don't match");
-//                request.getRequestDispatcher("create-account.jsp").forward(request, response);
-//            }
-//            
-//            
-//        } catch (RuntimeException e) {
-//            request.setAttribute("message", "Error: " + e.getMessage());
-//            request.getRequestDispatcher("error.jsp").forward(request, response);
-//        }
+  
+  
 
-    }
 
-    private void successAlert(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Establece atributos para mostrar el alert
-        request.setAttribute("showSuccessAlert", true);
-        request.setAttribute("alertTitle", "Welcome!");
-        request.setAttribute("alertText", "Now it’s your turn to rate all the movies you want! Go ahead, be the ultimate movie critic!");
-        
-        // Redirige de vuelta al mismo JSP del formulario
-        request.getRequestDispatcher("create-account.jsp").forward(request, response);
-    }
-    
-
-    /**
-     * Guarda un archivo de imagen en el servidor en un directorio designado.
-     *
-     * @param filePart el archivo cargado por el cliente como parte de la
-     * solicitud.
-     * @throws NullPointerException si el archivo o su nombre es nulo o vacío.
-     * @throws RuntimeException si ocurre un error de E/S durante el guardado.
-     */
-    private void saveImage(Part filePart) {
-        try {
-            // Validar si el archivo es nulo
-            if (filePart == null) {
-                throw new NullPointerException("Avatar is null.");
-            }
-
-            String fileName = getFileName(filePart);
-            if (fileName != null && !fileName.isEmpty()) {
-                // Guardar el archivo en el servidor
-                String uploadPath = getServletContext().getRealPath("") + File.separator + "uploads";
-                File uploadDir = new File(uploadPath);
-                if (!uploadDir.exists()) {
-                    uploadDir.mkdir(); // Crear el directorio si no existe
-                }
-                filePart.write(uploadPath + File.separator + fileName);
-            } else {
-                throw new NullPointerException("File name is null or empty.");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Error saving the image: " + e.getMessage(), e);
-        }
     }
 
     /**
-     * Extrae el nombre del archivo del encabezado content-disposition de la
-     * parte cargada.
+     * Returns a short description of the servlet.
      *
-     * @param part el objeto Part que contiene el archivo y sus metadatos.
-     * @return el nombre del archivo, o null si no se encuentra.
+     * @return a String containing servlet description
      */
-    private String getFileName(Part part) {
-        String contentDisposition = part.getHeader("content-disposition");
-        for (String content : contentDisposition.split(";")) {
-            if (content.trim().startsWith("filename")) {
-                return content.substring(content.indexOf("=") + 2, content.length() - 1);
-            }
-        }
-        return null;
-    }
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 
 }
