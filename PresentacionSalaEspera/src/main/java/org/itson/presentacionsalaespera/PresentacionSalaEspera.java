e7d6f90bffca6dd729d050724a544cb24e15f1af
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-12-01 11:33:42 -0700

    error 500

---
+++ PresentacionSalaEspera/src/main/java/org/itson/presentacionsalaespera/PresentacionSalaEspera.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.presentacionsalaespera;
+
+/**
+ *
+ * @author hisam
+ */
+public class PresentacionSalaEspera {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
